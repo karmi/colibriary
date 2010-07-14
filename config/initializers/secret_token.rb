5147964fa77545b810de977dce65d3ef1bfc2ce8
Karel Minarik <karmi@karmi.cz>
2010-07-14 18:35:32 +0200

    Initial commit -- Prazdna Rails 3 aplikace

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = '81a3b30fd420f09d6f02152b980b33a1ee6fce28a6bf7d49db59d8a0471e40d37ead6ea47da9f0e7990a68fb52e1baaf2784dc56a68c27e4c5675a9f42d989d6'
