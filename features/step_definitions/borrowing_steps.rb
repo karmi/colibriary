d38e67e87f96a6365197d522abf8efead6dd5349
Karel Minarik <karmi@karmi.cz>
2010-07-14 21:24:32 +0200

    Pridana specifikace kroku "pokud existuje vytisk knihy ..."

--- features/step_definitions/borrowing_steps.rb
+++ features/step_definitions/borrowing_steps.rb
@@ -1,3 +1,8 @@
 Pokud /^existuje čtenář.. jménem "([^"]*)" s platným průkazem$/ do |name|
   Reader.create :name => name, :card_id => 123
 end
+
+Pokud /^existuje neprezenční výtisk knihy "([^"]*)"$/ do |book|
+  author, title = book.split(':')
+  Book.create :author => author.strip, :title => title.strip, :borrowable => true
+end
