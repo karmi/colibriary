dd0f532adeb83cd30c6914a1fd3a8547a224343d
Karel Minarik <karmi@karmi.cz>
2010-07-15 12:26:04 +0200

    Pridana specifikace kroku "kdyz vyplnim cislo prukazu ..."

--- features/step_definitions/borrowing_steps.rb
+++ features/step_definitions/borrowing_steps.rb
@@ -10,3 +10,10 @@ end
 Pokud /^jsem na stránce "Knihy"$/ do
   visit books_path
 end
+
+Když /^vyplním číslo průkazu čtenářky jménem "([^"]*)" u knihy "([^"]*)"$/ do |reader_name, book_name|
+  author, title = book_name.split(':')
+  reader = Reader.find_by_name(reader_name)
+  book   = Book.find_by_author_and_title(author.strip, title.strip)
+  fill_in dom_id(book, :borrower_card_id_for), :with => reader.card_id     #borrower_card_id_for_book_1
+end
